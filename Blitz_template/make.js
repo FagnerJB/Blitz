4ce5c6d0dc2bda67e1775bbc98f89475b77e8aec
Fagner JB <para@fagnerjb.com>
2026-01-28 13:47:08 -0300

    🔥 Cleaning

--- Blitz_template/make.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const epubZip = require('epub-zip');
- 
-const content = epubZip("Blitz_template/Blitz_epub3");
-
-fs.writeFileSync("Blitz_template/Blitz_epub3.epub", content);
